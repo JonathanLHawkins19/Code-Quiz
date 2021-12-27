2e588e78a5d0018e1694d6ab0f50977c2af28f82
JonathanLHawkins19 <hawkins.jonathan19@gmail.com>
2021-12-26 21:04:24 -0500

    Delete Assets

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// 1. When you click the start button a timer starts and you are presented with a question.
+// 2. When you answer a question then you are presented with another question.
+// 3. When you answer a question incorrectly time is subtracted from the clock.
+// 4. After all questions are answered or the timer reaches 0 the game is over.
+// 5. Once the games is over you can save your initials and score.
+
+// What is the correct syntax to close the title element?
+// </title>
+// <!title>
+// <?title>
+// <endtitle>
+
+// What is the purpose of the body element?
+// The body element connects to the head element.
+// This is where all od the webpage's content will go. 
+// The body element will an empy placeholder.
+
+// What does HTML stand for?
+// Hyper Text Multiple Language
+// Hyper Text Markup Language
+// Hyper Text Preprocessor
+// Hyper Tool Multi Language
+
+// What does CSS stand for?
+// Computer Style Sheet 
+// Common Style Sheet
+// Cascading Style Sheet
+// Colorful Style Sheet
