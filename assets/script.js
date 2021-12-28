b66371afaa66b7c614f6a34542e12af389220bcf
JonathanLHawkins19 <hawkins.jonathan19@gmail.com>
2021-12-27 21:35:04 -0500

    Code-Quiz Done

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
+const startButton = document.getElementById("start")
+
+startButton.addEventListener('click', startQuiz)
+
+function startGame() {
+    console.log("Started")
+}
+
 // 1. When you click the start button a timer starts and you are presented with a question.
 // 2. When you answer a question then you are presented with another question.
 // 3. When you answer a question incorrectly time is subtracted from the clock.
@@ -5,10 +13,10 @@
 // 5. Once the games is over you can save your initials and score.
 
 // What is the correct syntax to close the title element?
-// </title>
-// <!title>
-// <?title>
-// <endtitle>
+// /title
+// !title
+// ?title
+// endtitle
 
 // What is the purpose of the body element?
 // The body element connects to the head element.
@@ -26,3 +34,9 @@
 // Common Style Sheet
 // Cascading Style Sheet
 // Colorful Style Sheet
+
+// Which of the following is NOT a good reason for version control?
+// Version control allows the codebase to be modified and tested without interrupting the user experience.
+// Version control allows changes to the codebase to be tested individually.
+// Version control allows teams to work on individual features synchronously.
+// Version control allows features to ship directly to the main branch.
